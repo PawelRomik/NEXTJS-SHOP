1fe8d45bd80954b1cdbb0c9451f9c3753f106ee9
Paweł Romik <leoneczek15@gmail.com>
2024-05-03 13:07:38 +0200

    Added connection to GraphQL Strapi.

--- app/(public)/(shop)/page.tsx
+++ app/(public)/(shop)/page.tsx
@@ -1,18 +1,78 @@
 import { Grid } from "@radix-ui/themes";
 import ProductDisplay from "../../components/ProductDisplay";
-import products from "../../data/products";
+import { gql, ApolloQueryResult } from "@apollo/client";
+import createApolloClient from "../../../apollo-client";
 
-export default function HomePage() {
+interface ProductData {
+	id: string;
+	attributes: {
+		name: string;
+		price: number;
+		image: {
+			data: {
+				attributes: {
+					url: string;
+				};
+			};
+		};
+		categories: {
+			data: {
+				attributes: {
+					name: string;
+				};
+			}[];
+		};
+	};
+}
+
+interface QueryResult {
+	products: {
+		data: ProductData[];
+	};
+}
+
+const GET_PRODUCTS = gql`
+	query {
+		products {
+			data {
+				id
+				attributes {
+					name
+					price
+					image {
+						data {
+							attributes {
+								url
+							}
+						}
+					}
+					categories {
+						data {
+							attributes {
+								name
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`;
+
+export default async function HomePage() {
+	const client = createApolloClient();
+	const { data }: ApolloQueryResult<QueryResult> = await client.query({ query: GET_PRODUCTS });
+
 	return (
 		<main className="flex-1 p-6	">
 			<Grid gap="4" width="auto" className="grid-cols-1 p-6 lg:grid-cols-4">
-				{products.map((product) => (
+				{data.products.data.map((product) => (
 					<ProductDisplay
 						id={product.id}
-						name={product.name}
-						price={product.price}
-						category={product.category}
-						imageUrl={product.imageUrl}
+						name={product.attributes.name}
+						price={product.attributes.price}
+						category={""}
+						imageUrl={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
 						key={product.id}
 					></ProductDisplay>
 				))}
