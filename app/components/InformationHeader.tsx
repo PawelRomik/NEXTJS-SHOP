db7d891867c9194705a281c226e566799d7f7096
Paweł Romik <leoneczek15@gmail.com>
2024-07-12 22:55:20 +0200

    Changed information pages

--- app/components/InformationHeader.tsx
+++ app/components/InformationHeader.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
 import ShopLogo from "./ShopLogo";
 
 export default function InformationHeader() {
 	return (
-		<header className="sticky top-0 z-10 mx-auto flex w-full items-center justify-between border-b-2 bg-white p-4">
+		<header className="sticky top-0 z-10 mx-auto box-border flex h-full w-full items-center justify-between border-b-[3px] border-b-red-600 bg-black p-4 text-white lg:px-[6rem]">
+			<div className="flex items-center justify-start">
+				<Link href="/" title="Shop">
+					<button className="flex lg:hidden">
+						<i className="ri-home-2-line text-2xl"></i>
+					</button>
+				</Link>
+			</div>
 			<ShopLogo />
+			<div className="hidden h-full w-1/3 items-center justify-center lg:flex">
+				<Link href="/" title="Shop">
+					<p className="hover:border-b-2 hover:border-red-600 hover:text-red-600 ">SHOP</p>
+				</Link>
+			</div>
+
+			<div className="flex h-full items-center justify-end gap-5 lg:w-1/3"></div>
 		</header>
 	);
 }
--- app/components/ShopLogo.tsx
+++ app/components/ShopLogo.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 export default function ShopLogo() {
 	return (
-		<Link href="/" title="N3XT SHOP" className="w-1/3">
-			<div className="flex w-full items-center justify-start gap-2 lg:w-auto">
+		<div className=" lg:w-1/3">
+			<Link href="/" title="N3XT SHOP" className="flex items-center justify-start gap-2 lg:w-12">
 				<Image src={nextLogo} className="hidden w-12 lg:block" alt="shop logo" />
 				<h1 className="text-3xl font-bold italic">Ephonix</h1>
-			</div>
-		</Link>
+			</Link>
+		</div>
 	);
 }
