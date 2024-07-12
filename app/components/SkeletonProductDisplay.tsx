ee693c39968e8fa6a923755f9b2186891a212f9c
Paweł Romik <leoneczek15@gmail.com>
2024-07-12 22:56:05 +0200

    Changed skeleton color

--- app/components/SkeletonProductDisplay.tsx
+++ app/components/SkeletonProductDisplay.tsx
@@ -5,7 +5,7 @@ export default function SkeletonProductDisplay() {
 			aria-busy="true"
 			className="h-[300px] flex-[0_0_100%] lg:h-[400px] lg:flex-[1_0_33%]"
 		>
-			<span className="inline-flex h-full w-full animate-pulse select-none rounded-md bg-zinc-100 leading-none">
+			<span className="inline-flex h-full w-full animate-pulse select-none rounded-md bg-black leading-none">
 				‌
 			</span>
 			<br />
