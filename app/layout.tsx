eade1d9e852d8cadd281ba1a10b207788062e227
Paweł Romik <leoneczek15@gmail.com>
2024-07-12 19:38:22 +0200

    Changed scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={`${inter.className} custom-scrollbar`}>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
