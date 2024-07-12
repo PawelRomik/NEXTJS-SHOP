e5bf5bb264bbfe9f2b9b8cfc9723b21a29b43f9e
Paweł Romik <leoneczek15@gmail.com>
2024-07-12 22:20:55 +0200

    added padding to header

--- app/components/ShopHeader.tsx
+++ app/components/ShopHeader.tsx
@@ -8,7 +8,7 @@ import SearchBar from "./SearchBar";
 
 export default function ShopHeader() {
 	return (
-		<header className="sticky top-0 z-10 mx-auto box-border flex h-full w-full items-center justify-between border-b-[3px] border-b-red-600 bg-black p-4 text-white">
+		<header className="sticky top-0 z-10 mx-auto box-border flex h-full w-full items-center justify-between border-b-[3px] border-b-red-600 bg-black p-4 text-white lg:px-[6rem]">
 			<Hamburger />
 			<ShopLogo />
 			<nav className="hidden h-full w-1/3 items-center justify-center lg:flex">
