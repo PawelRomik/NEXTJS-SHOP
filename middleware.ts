e44f156445408db5c50ffd79b0f13ea656c742af
Paweł Romik <leoneczek15@gmail.com>
2024-08-08 18:03:13 +0200

    fixed ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { i18nRouter } from "next-i18n-router";
 import i18nConfig from "./i18nConfig";
+import { NextFetchEvent, NextRequest } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/order(.*)"]);
 
@@ -8,7 +9,7 @@ const clerkAuthMiddleware = clerkMiddleware((auth, req) => {
 	if (isProtectedRoute(req)) auth().protect();
 });
 
-export default function combinedMiddleware(request, event) {
+export default function combinedMiddleware(request: NextRequest, event: NextFetchEvent) {
 	const clerkResponse = clerkAuthMiddleware(request, event);
 	if (clerkResponse) return clerkResponse;
 	return i18nRouter(request, i18nConfig);
