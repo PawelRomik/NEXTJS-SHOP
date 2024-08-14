2973b69801f1eb2c788b8418a9d61e5e7362050f
Paweł Romik <leoneczek15@gmail.com>
2024-08-14 18:21:07 +0200

    fixed vercel error

--- app/[locale]/(private)/layout.tsx
+++ app/[locale]/(private)/layout.tsx
@@ -1,20 +1,30 @@
-import { Inter } from "next/font/google";
 import Footer from "../../components/Footer";
 import ShopHeader from "../../components/ShopHeader";
 import ScrollToTopButton from "../../components/ScrollToTopButton";
+import initTranslations from "../../i18n";
+import TranslationsProvider from "../../components/TranslationProvider";
 
-const inter = Inter({
-	subsets: ["latin"],
-	display: "swap"
-});
+type informationLayoutProps = {
+	children: React.ReactNode;
+	params: { locale: string };
+};
 
-export default function ShopLayout({ children }: { children: React.ReactNode }) {
+export default async function InformationLayout({ children, params }: informationLayoutProps) {
+	const { locale } = params;
+	const { resources } = await initTranslations(locale, ["common", "shop"]);
+
 	return (
-		<div className={`${inter.className} flex min-h-screen flex-col`}>
-			<ShopHeader />
-			{children}
-			<ScrollToTopButton />
-			<Footer />
-		</div>
+		<TranslationsProvider
+			namespaces={["common", "information"]}
+			locale={locale}
+			resources={resources}
+		>
+			<div className={`roboto flex min-h-screen flex-col`}>
+				<ShopHeader locale={locale} />
+				{children}
+				<ScrollToTopButton />
+				<Footer locale={locale} />
+			</div>
+		</TranslationsProvider>
 	);
 }
