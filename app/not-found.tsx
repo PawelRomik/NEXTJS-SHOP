5784effcb62a99e45a87025f02e9753d99a196bf
Paweł Romik <leoneczek15@gmail.com>
2024-08-19 21:18:18 +0200

    testing fix for vercel

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import ShopHeader from "./components/ShopHeader";
