ef4cc4acab1fef920d5490bf72fe306365a920d3
Paweł Romik <leoneczek15@gmail.com>
2025-01-12 18:16:32 +0100

    Changed main page

--- app/components/CategoryBanner.tsx
+++ app/components/CategoryBanner.tsx
@@ -2,6 +2,7 @@ import { getTranslations } from "next-intl/server";
 import productImage from "../../public/i1.png";
 import Image from "next/image";
 import * as motion from "framer-motion/client";
+import Link from "next/link";
 
 const item = {
 	visible: { x: 0 },
@@ -19,16 +20,36 @@ export default async function CategoryBanner({ category }: ProductOtherSectionPr
 		<motion.div
 			variants={item}
 			transition={{ duration: 0.5, ease: "easeInOut" }}
-			className="shadow-inset relative z-[20] flex h-[250px] w-full flex-col items-center justify-center gap-10 bg-red-600 lg:flex-row lg:gap-5"
+			className="shadow-inset glow-effect relative z-[20]   flex h-[400px] w-full flex-col items-center justify-between overflow-hidden bg-zinc-900 py-3 lg:flex-row lg:gap-5"
 		>
-			<Image src={productImage} className="h-[40%] w-auto p-5 lg:h-full" alt={category}></Image>
-			<div className=" flex h-full flex-col items-center justify-center gap-5">
-				<h2 className="text-shadow text-2xl font-bold uppercase text-white lg:text-4xl">
-					{t(category)}
-				</h2>
-				<p className="text-shadow text-xl font-bold uppercase text-white lg:text-2xl">
-					Super Ceny!
+			<div className=" ml-[10%] flex h-full w-[500px] flex-col items-start justify-around text-white">
+				<h2 className="text-5xl font-bold uppercase">{t(category)}</h2>
+				<p className="w-[50%]">
+					Explore the latest tech and gaming gear, featuring cutting-edge components and accessories
+					designed to elevate your experience. Stay ahead with innovation at your fingertips.
 				</p>
+				<Link href="/new">
+					<button className="w-[300px] rounded-lg bg-red-600 p-4 text-2xl font-bold text-white hover:bg-red-500">
+						Check now
+					</button>
+				</Link>
+			</div>
+			<Image
+				src={productImage}
+				className="img absolute left-[50%] flex h-[40%] w-auto translate-x-[-50%] self-center p-5 lg:h-full"
+				alt={category}
+			></Image>
+			<div className=" mr-[10%] flex h-full w-[500px] flex-col items-end justify-around text-right text-white">
+				<p></p>
+				<p className="w-[50%]">
+					Explore the latest tech and gaming gear, featuring cutting-edge components and accessories
+					designed to elevate your experience. Stay ahead with innovation at your fingertips.
+				</p>
+				<Link href="/new">
+					<button className="w-[300px] rounded-lg bg-red-600 p-4 text-2xl font-bold text-white hover:bg-red-500">
+						Check now
+					</button>
+				</Link>
 			</div>
 		</motion.div>
 	);
