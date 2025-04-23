b304ae415b83ff6123efa8e38a97f74131534045
Paweł Romik <leoneczek15@gmail.com>
2025-04-23 20:35:41 +0200

    Changed information pages styles

--- app/[locale]/(public)/(information)/contact/page.tsx
+++ app/[locale]/(public)/(information)/contact/page.tsx
@@ -2,6 +2,8 @@ import { revalidatePath } from "next/cache";
 import { Metadata } from "next";
 import { useTranslations } from "next-intl";
 import { getTranslations } from "next-intl/server";
+import Image from "next/image";
+import logo from "../../../../../public/logolg.png";
 
 export async function generateMetadata({
 	params: { locale }
@@ -16,26 +18,40 @@ export async function generateMetadata({
 }
 
 export default function ContactPage() {
-	const t = useTranslations("information");
+	const t = useTranslations();
 	revalidatePath("/[locale]/contact", "page");
 
 	return (
-		<main className="mx-auto flex flex-1 flex-col justify-start gap-6 p-6">
-			<div className="flex text-4xl font-bold text-red-600">
-				<i className="ri-arrow-right-double-fill"></i>
-				<h1>{t("contact")}</h1>
+		<main className="flex flex-1 flex-col justify-start ">
+			<div className="my-3 flex bg-red-600 px-6 py-3 text-4xl font-bold uppercase">
+				<h1>{t("information.contact")}</h1>
 			</div>
-			<p>
-				<i className="ri-phone-fill"></i> 123 456 789
-			</p>
-			<p>
-				<i className="ri-mail-fill"></i> N3xt@gmail.com
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
-				viverra, non vestibulum lorem luctus. Curabitur a urna in nulla aliquam ultrices ac ut
-				ipsum.
-			</p>
+			<div className="flex h-full w-full">
+				<div className="flex h-full flex-[3]">
+					<div>
+						<p className="bg-[rgb(16,16,16)] p-2 px-6">
+							<i className="ri-phone-fill"></i> 123 456 789
+						</p>
+						<p className="bg-[rgb(8,8,8)] p-2  px-6">
+							<i className="ri-mail-fill"></i> N3xt@gmail.com
+						</p>
+						<p className="bg-[rgb(16,16,16)] p-2  px-6">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu
+							ante viverra, non vestibulum lorem luctus. Curabitur a urna in nulla aliquam ultrices
+							ac ut ipsum.
+						</p>
+					</div>
+					<div className="flex h-full flex-1 items-center justify-center overflow-hidden bg-[rgb(16,16,16)]">
+						<Image
+							src={logo}
+							width={400}
+							height={400}
+							className=" h-full object-contain"
+							alt={t("common.shopLogo")}
+						/>
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
--- app/[locale]/(public)/(information)/privacy/page.tsx
+++ app/[locale]/(public)/(information)/privacy/page.tsx
@@ -19,13 +19,15 @@ export default function PrivacyPage() {
 	revalidatePath("/[locale]/privacy", "page");
 
 	return (
-		<main className="mx-auto flex flex-1 flex-col justify-start gap-6 p-6">
-			<div className="flex text-4xl font-bold text-red-600">
-				<i className="ri-arrow-right-double-fill"></i>
+		<main className="flex flex-1 flex-col justify-start ">
+			<div className="my-3 flex bg-red-600 px-6 py-3 text-4xl font-bold uppercase">
 				<h1>{t("privacy")}</h1>
 			</div>
+			<p className="mb-2 border-l-2 border-red-600 bg-[rgb(8,8,8)] px-6 font-bold uppercase italic">
+				{t("lastUpdate")} 23.04.2025
+			</p>
 			<ul>
-				<li>
+				<li className="bg-[rgb(16,16,16)] p-2">
 					<h2 className="p-3 font-bold">1. {t("generalInformation")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -33,7 +35,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(8,8,8)] p-2">
 					<h2 className="p-3 font-bold">2. {t("personalDataAdministrator")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -41,7 +43,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(16,16,16)] p-2">
 					<h2 className="p-3 font-bold">3. {t("personalData")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -49,7 +51,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(8,8,8)] p-2">
 					<h2 className="p-3 font-bold">4. {t("basisForDataProcessing")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -57,7 +59,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(16,16,16)] p-2">
 					<h2 className="p-3 font-bold">5. {t("dataRecipients")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -65,7 +67,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(8,8,8)] p-2">
 					<h2 className="p-3 font-bold">6. {t("userRights")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -73,7 +75,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(16,16,16)] p-2">
 					<h2 className="p-3 font-bold">7. {t("dataSecurity")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -81,7 +83,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(8,8,8)] p-2">
 					<h2 className="p-3 font-bold">8. {t("dataRetentionPeriod")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -89,7 +91,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(16,16,16)] p-2">
 					<h2 className="p-3 font-bold">9. {t("changesToPrivacyPolicy")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -97,7 +99,7 @@ export default function PrivacyPage() {
 						ipsum.
 					</p>
 				</li>
-				<li>
+				<li className="bg-[rgb(8,8,8)] p-2">
 					<h2 className="p-3 font-bold">10. {t("contact")}</h2>
 					<p className="pl-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu ante
@@ -106,11 +108,6 @@ export default function PrivacyPage() {
 					</p>
 				</li>
 			</ul>
-			<hr></hr>
-			<p>
-				<strong>{t("lastUpdate")}:</strong> 23.08.2024
-			</p>
-			<hr></hr>
 		</main>
 	);
 }
