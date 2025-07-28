fcb3bcf541da54b197ebfe79ac02035709353d47
Paweł Romik <leoneczek15@gmail.com>
2025-07-28 07:18:59 +0200

    Added orders to hamburger menu

--- shop/app/components/HamburgerAccountSection.tsx
+++ shop/app/components/HamburgerAccountSection.tsx
@@ -33,7 +33,7 @@ export default function HamburgerAccountSection() {
 			<div className="mb-3 text-2xl tracking-wider">
 				<SignedIn>
 					<div className="flex w-full flex-col items-center justify-center gap-2">
-						<div className="flex w-full items-center justify-center gap-5 font-bold ">
+						<div className="flex w-full flex-wrap items-center justify-center gap-5 font-bold ">
 							<Link href={"/user/settings"} title={t("hamburger.settings")}>
 								<button className=" flex gap-1 border-2 border-red-600 bg-[rgb(11,11,11)] p-1 px-4 text-base md:p-2 md:px-6 md:text-xl">
 									<i className="ri-settings-2-line"></i>
