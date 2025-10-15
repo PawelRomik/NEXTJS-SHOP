40e420b3371360bd2fd639291207555ca687874e
Paweł Romik <leoneczek15@gmail.com>
2025-10-15 17:39:20 +0200

    Refactor: Filters

--- shop/app/components/filter/Filter.tsx
+++ shop/app/components/filter/Filter.tsx
@@ -1,33 +1,16 @@
+"use client";
+
 type FilterProps = {
-	filter: {
-		id: string;
-		attributes: {
-			name: string;
-			tags: {
-				data: {
-					attributes: {
-						name: string;
-					};
-				}[];
-			};
-		};
-	};
+	name: string;
+	tag: string;
 };
 
-export default function Filter({ filter }: FilterProps) {
+export default function Filter({ name, tag }: FilterProps) {
+	const id = `${name}-${tag}`;
 	return (
-		<div className="flex w-[300px] flex-col rounded-2xl bg-[rgb(24,24,24)]   px-10  py-2 lg:w-full">
-			<h2 className="mb-2 w-full border-b-4 border-zinc-800 p-2 font-bold  uppercase">
-				{filter.attributes.name}
-			</h2>
-			<div className="text-zinc-400">
-				{filter.attributes.tags.data.map((tag, index) => (
-					<div key={index} className="flex items-center justify-start gap-2 text-white">
-						<input type="checkbox" name={"tags"} value={tag.attributes.name} />
-						{tag.attributes.name}
-					</div>
-				))}
-			</div>
+		<div className="flex items-center justify-start gap-2 text-white">
+			<input id={id} type="checkbox" name="tags" value={tag} />
+			<label htmlFor={id}>{tag}</label>
 		</div>
 	);
 }
--- shop/app/components/filter/SortOptions.tsx
+++ shop/app/components/filter/SortOptions.tsx
@@ -4,21 +4,32 @@ import { useTranslations } from "next-intl";
 export default function SortOptions() {
 	const t = useTranslations("filters");
 
+	const options = [
+		{ value: "latest", label: t("sortNewest") },
+		{ value: "priceLow", label: t("sortPriceToHigh") },
+		{ value: "priceHigh", label: t("sortPriceToLow") },
+		{ value: "nameStart", label: t("sortNameToZ") },
+		{ value: "nameEnd", label: t("sortNameToA") }
+	];
+
 	return (
 		<div className="flex h-full w-[300px] flex-col rounded-2xl bg-[rgb(24,24,24)]  px-10  py-2 lg:w-full">
-			<h2 className="mb-2 w-full border-b-4 border-zinc-800 p-2 font-bold  uppercase">
+			<label
+				htmlFor="sort"
+				className="mb-2 w-full border-b-4 border-zinc-800 p-2 font-bold  uppercase"
+			>
 				{t("sort")}
-			</h2>
+			</label>
 			<select
 				name="sort"
 				defaultValue={"latest"}
 				className="border-none bg-zinc-800 p-2 pr-3 text-center outline-none  focus:bg-zinc-900"
 			>
-				<option value={"latest"}>{t("sortNewest")}</option>
-				<option value={"priceLow"}>{t("sortPriceToHigh")}</option>
-				<option value={"priceHigh"}>{t("sortPriceToLow")}</option>
-				<option value={"nameStart"}>{t("sortNameToZ")}</option>
-				<option value={"nameEnd"}>{t("sortNameToA")}</option>
+				{options.map(({ value, label }) => (
+					<option key={value} value={value}>
+						{label}
+					</option>
+				))}
 			</select>
 		</div>
 	);
