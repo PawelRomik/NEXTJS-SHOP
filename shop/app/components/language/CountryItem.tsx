c4208164cac8250e48000d74c365b56bec0f1f25
Paweł Romik <leoneczek15@gmail.com>
2025-10-16 14:39:33 +0200

    Refactor: LanguageSwitcher

--- shop/app/components/language/CountryItem.tsx
+++ shop/app/components/language/CountryItem.tsx
@@ -2,19 +2,20 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { usePathname } from "next/navigation";
 import Flag from "./Flag";
+import Link from "next/link";
 
 export default function CountryItem({ country }: { country: string }) {
 	const pathname = usePathname().substring(3);
 
 	return (
 		<DropdownMenuItem className="p-0">
-			<a
+			<Link
 				href={`/${country}${pathname}`}
 				className="flex w-full cursor-pointer items-center justify-center gap-2 p-1 font-bold  transition hover:bg-red-600 hover:text-white"
 			>
 				<Flag country={country} />
 				<p className="bold uppercase">{country}</p>
-			</a>
+			</Link>
 		</DropdownMenuItem>
 	);
 }
--- shop/app/components/language/Flag.tsx
+++ shop/app/components/language/Flag.tsx
@@ -1,16 +1,10 @@
 import { PL, US } from "country-flag-icons/react/3x2";
 
-export default function Flag({ country }: { country: string }) {
-	const getFlag = () => {
-		switch (country) {
-			case "pl":
-				return <PL title="Polski" className="w-[1.5rem]" />;
-			case "en":
-				return <US title="English" className="w-[1.5rem]" />;
-			default:
-				return <US title="English" className="w-[1.5rem]" />;
-		}
-	};
+const FLAGS: Record<string, JSX.Element> = {
+	pl: <PL title="Polski" className="w-[1.5rem]" />,
+	en: <US title="English" className="w-[1.5rem]" />
+};
 
-	return getFlag();
+export default function Flag({ country }: { country: string }) {
+	return FLAGS[country] ?? FLAGS["en"];
 }
